83b4d8f3432759a7ff4d62fe38b62f5cc9b351e1
askloonline-maker <asklo.online@gmail.com>
2026-06-23 21:59:01 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Ensure all internal routes and static components are excluded safely
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$$).*)",
+    '/((?!_next|[^?]*\\.[^?]*$$).*)',
+    '/(api|trpc)(.*)',
   ],
 };
