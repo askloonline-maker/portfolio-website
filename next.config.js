f4286ef036d8a9dcbbf580f1fe868b5270406ced
askloonline-maker <asklo.online@gmail.com>
2026-06-23 21:51:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: 'pk_live_Y2xlbXN1YXNrbG8ub25saW5lLndlYm1hbmlmZXN0LmluZm86M21wOWhzcTV0MXMwodH8RAFKX0E',
-  },
+  /* config options here */
 };
 
 module.exports = nextConfig;
