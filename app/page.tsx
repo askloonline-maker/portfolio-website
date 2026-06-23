f76f2918a102c21965f61b239bb0a35a87c4fd1f
askloonline-maker <asklo.online@gmail.com>
2026-06-23 22:37:23 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,32 +9,23 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-slate-100">
-      {/* Navbar */}
       <Navbar />
-
-      {/* Hero Section */}
       <Hero />
 
-      {/* Main Layout */}
       <div className="max-w-7xl mx-auto px-4 py-6">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-
-          {/* Left Sidebar */}
           <div className="hidden lg:block lg:col-span-3">
             <Sidebar />
           </div>
 
-          {/* Feed */}
           <div className="lg:col-span-6 space-y-6">
             <CreatePost />
             <RecentQuestions />
           </div>
 
-          {/* Right Sidebar */}
           <div className="hidden lg:block lg:col-span-3">
             <RightSidebar />
           </div>
-
         </div>
       </div>
 
