9cb6bcc2552b9193f31ae94c3e6f3c8ec8edcace
askloonline-maker <asklo.online@gmail.com>
2026-06-26 23:19:14 +0530

    Delete app/space/[id] directory

--- app/space/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SpacePage({ params }: { params: { id: string } }) {
-  return (
-    <div>
-      <h1>Space: {params.id}</h1>
-      {/* Space page content */}
-    </div>
-  );
-}
