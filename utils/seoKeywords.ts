ed8aefa81d994040b94babd8bcfc131a2765b262
askloonline-maker <asklo.online@gmail.com>
2026-07-11 17:26:31 +0530

    Create seoKeywords.ts in utils folder

---
+++ utils/seoKeywords.ts
@@ -0,0 +1,33 @@
+export const seoKeywords = {
+  default: [
+    'portfolio',
+    'web development',
+    'frontend',
+    'backend',
+    'full stack',
+  ],
+  home: [
+    'portfolio',
+    'developer',
+    'web development',
+    'projects',
+  ],
+  about: [
+    'about',
+    'biography',
+    'experience',
+    'skills',
+  ],
+  projects: [
+    'projects',
+    'portfolio projects',
+    'web projects',
+    'case studies',
+  ],
+  contact: [
+    'contact',
+    'hire me',
+    'get in touch',
+    'services',
+  ],
+};
